c4c15375354843b3b3e7054b8f868209a54bb1fe
nishigandha30 <nish8800@gmail.com>
2018-05-10 18:56:28 +0530

    auto name

--- dpioffice/custom_methods.py
+++ dpioffice/custom_methods.py
@@ -2,14 +2,16 @@ import frappe
 from frappe.model.mapper import get_mapped_doc
 from frappe import throw, _
 from frappe.model.naming import make_autoname
-
+from datetime import datetime
 
 @frappe.whitelist()
 def set_task_autoname(doc, method):
+	frappe.msgprint("testhello")
 	project_abbr = frappe.db.get_value("Project",doc.project,"abbr")
 	if project_abbr:
-		doc.name =make_autoname("TASK-"+project_abbr + '-.#####')
+		now = datetime.now()
+		doc.name =make_autoname("TASK-" + project_abbr +"-"+ str(now.month) +"-"+ str(now.year) + '-.#####')
 	else:
 		doc.name = make_autoname("TASK" + '.#####')
-
+ 
 
